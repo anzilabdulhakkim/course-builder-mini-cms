361c3733c6f38bd9f01edfb3cba34069609f990f
ANZIL ABDUL HAKKIM <anzilabdulhakkim135@gmail.com>
2025-11-13 16:15:19 +0530

    init vite react app with tailwind configuration, router, axios and uuid

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className=" text-center text-white bg-gray-900 min-h-screen flex flex-col justify-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
